8e38e8608081a9e0282eb0e45b8172ba881afed1
Prathamesh Sonar <prathamesh@phoenixgen.com>
2020-06-22 11:19:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chatapp/helper/authenticate.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF145C9E),
+        scaffoldBackgroundColor: Color(0xFF1F1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Authenticate(),
+    );
+  }
+}
+
---
+++ lib/services/database.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class DatabaseMethods {
+  getUserByUserName(String username) async {
+    return await Firestore.instance.collection('users')
+        .where('name', isEqualTo: username)
+        .getDocuments();
+  }
+
+  uploadUserInfo(userMap) {
+    Firestore.instance.collection('users')
+        .add(userMap).catchError((e){
+      print(e.toString());
+    });
+
+  }
+}
